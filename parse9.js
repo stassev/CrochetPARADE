4f14acf2c06f95d449fc0265003d52aa50ab444a
Svetlin Tassev <stassev@runningonphysics.org>
2025-02-21 10:31:52 -0500

    Update mesh8.js, parse8.js, and 3 more files...

--- parse9.js
+++ parse9.js
@@ -3084,7 +3084,7 @@ function export_to_dot(Stitches, json) {
     //var json = null
     var JACS = [];
     if (json !== '') {
-        //json = JSON.parse8.json0)
+        //json = JSON.parse9.json0)
         for (var o of json.objects) {
             var pos = o.pos.split(',').map(Number);
             if (pos.length == 2) {
