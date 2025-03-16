3251665e54af09eb49c9fecca4facbb1f1d1001a
Svetlin Tassev <stassev@runningonphysics.org>
2025-03-16 06:42:38 -0400

    Update parse31.js, mesh31.js, and 5 more files...

--- mesh32.js
+++ mesh32.js
@@ -31,7 +31,7 @@ import {
     returnRotational,
     returnTranslation,
     setRotationAngles
-} from './transform_controls31.js';
+} from './transform_controls32.js';
 
 //import {
 //    SVGRenderer
@@ -186,7 +186,7 @@ export default function Generate3DModel(json0, renderer, scene, scene1, backgrou
     //    rendererSVG.domElement.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
 
 
-    var str = JSON.parse(JSON.stringify(json0)); //JSON.parse31.json0);
+    var str = JSON.parse(JSON.stringify(json0)); //JSON.parse32.json0);
     console.log(str);
 
     const uniqueLabels = new Set(); // Use a Set to store unique labels
