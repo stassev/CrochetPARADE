c454cca14e63fe5870e9b98023742e64d4981b77
Svetlin Tassev <stassev@runningonphysics.org>
2025-03-23 06:04:21 -0400

    Update sphere-generator40.js

--- sphere-generator40.js
+++ sphere-generator40.js
@@ -94,8 +94,9 @@ function cyclicPermuteBySC(list) {
   //if (a.length === 0) return list;
   
   Si++;
-  var count = Math.ceil(list.length / 18) * (Si % 3) + (list.length > 36 ? Si : 0);
-  count=count % list.length;
+  const count = Math.ceil(list.length / 18) * (Si % 3);//
+  //  + (list.length > 36 ? Si : 0);
+  //count=count % list.length;
   // RotateLeft in JavaScript
   return [...list.slice(count), ...list.slice(0, count)];
 }
