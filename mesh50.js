4f1c92fd3c7d42df84b2f37f5072a5c19bf1f78d
Svetlin Tassev <stassev@runningonphysics.org>
2025-04-08 04:33:48 -0400

    Update bezier49.js, mesh49.js, and 11 more files...

--- mesh50.js
+++ mesh50.js
@@ -31,7 +31,7 @@ import {
     returnRotational,
     returnTranslation,
     setRotationAngles
-} from './transform_controls49.js';
+} from './transform_controls50.js';
 
 //import {
 //    SVGRenderer
@@ -187,7 +187,7 @@ var exportSVGinColor=false;
     //    rendererSVG.domElement.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
 
 
-    var str = JSON.parse(JSON.stringify(json0)); //JSON.parse49.json0);
+    var str = JSON.parse(JSON.stringify(json0)); //JSON.parse50.json0);
     console.log(str);
 
     const uniqueLabels = new Set(); // Use a Set to store unique labels
