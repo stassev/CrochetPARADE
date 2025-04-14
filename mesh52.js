da97f8f1fa3eff9d67c45dadd4e409eb29c1b446
Svetlin Tassev <stassev@runningonphysics.org>
2025-04-14 14:32:09 -0400

    feat: improve scattering of increases and decreases in pattern generators.

--- mesh52.js
+++ mesh52.js
@@ -31,7 +31,7 @@ import {
     returnRotational,
     returnTranslation,
     setRotationAngles
-} from './transform_controls51.js';
+} from './transform_controls52.js';
 
 //import {
 //    SVGRenderer
@@ -187,7 +187,7 @@ var exportSVGinColor=false;
     //    rendererSVG.domElement.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
 
 
-    var str = JSON.parse(JSON.stringify(json0)); //JSON.parse51.json0);
+    var str = JSON.parse(JSON.stringify(json0)); //JSON.parse52.json0);
     console.log(str);
 
     const uniqueLabels = new Set(); // Use a Set to store unique labels
